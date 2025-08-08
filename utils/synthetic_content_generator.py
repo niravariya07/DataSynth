2e5547abf53ba5ec80b63b6cdfaa5e6f7ae2586b
niravariya07 <nidhiravariya256@gmail.com>
2025-08-08 22:12:27 +0530

    updated : synthetic_content_generator.py -> writing function

--- utils/synthetic_content_generator.py
+++ utils/synthetic_content_generator.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("GEMINI_API_KEY"))
 
 def data_generator_llm(
         columns: List[Dict[str, str]],
