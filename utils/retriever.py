9882b94aff60447066ad441b46fe9fb0b69801b1
niravariya07 <nidhiravariya256@gmail.com>
2025-08-08 18:58:05 +0530

    updated : retriever.py -> writing function

--- utils/retriever.py
+++ utils/retriever.py
@@ -1,4 +1,16 @@
 import faiss
 import numpy as np
-import pickle
-from sentence_transformers import SentenceTransformer
+from typing import Dict, List, Tuple
+from embedder import get_embedding
+
+def retrieve_context(
+        query: str,
+        index: faiss.IndexFlatL2,
+        mapping: Dict[int, Dict[str, str]],
+        top_k: int = 1
+) -> List[Dict[str, str]]:
+    query_vector = get_embedding(query).astype(np.float32).reshape(1, -1)
+
+    distances, indices = index.search(query_vector, top_k)
+
+    
