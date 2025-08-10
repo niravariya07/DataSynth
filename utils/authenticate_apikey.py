e46e2c661c6d29224006365b51b9f643fab0b480
niravariya07 <nidhiravariya256@gmail.com>
2025-08-10 23:08:32 +0530

    updated : authenticate_apikey.py -> imported libraries

--- utils/authenticate_apikey.py
+++ utils/authenticate_apikey.py
@@ -3,5 +3,5 @@ import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() 
 client = genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
