4aa55dcbe7a0d5ef9511903e30f647a4603f888b
niravariya07 <nidhiravariya256@gmail.com>
2025-08-10 23:10:55 +0530

    updated : authenticate_apikey.py -> imported libraries

--- utils/authenticate_apikey.py
+++ utils/authenticate_apikey.py
@@ -4,4 +4,7 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv() 
-client = genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+owner_api_key = genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+max_free_users = 5
+
+
