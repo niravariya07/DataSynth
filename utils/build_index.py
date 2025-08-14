9b55e1df3541403578413e56f6612eb6022c6ba9
niravariya07 <nidhiravariya256@gmail.com>
2025-08-14 23:30:25 +0530

    updated : build_index.py

--- utils/build_index.py
+++ utils/build_index.py
@@ -1,21 +1,35 @@
+from embedder import get_embeddings
 import faiss
 import numpy as np
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Union
 
-from embedder import get_embeddings
-
-def build_index(rag_entries: List[Dict[str, str]]) -> Tuple[faiss.IndexFlatL2, Dict[int, Dict[int,Dict[str, str]]]]:
-    if not rag_entries:
-        raise ValueError("RAG entries list is empty.")
+# def build_index(rag_entries: List[Dict[str, str]]) -> Tuple[faiss.IndexFlatL2, Dict[int, Dict[int,Dict[str, str]]]]:
+#     if not rag_entries:
+#         raise ValueError("RAG entries list is empty.")
     
-    texts = [f"{col['name']}: {col['description']}" for col in rag_entries]
+#     texts = [f"{col['name']}: {col['description']}" for col in rag_entries]
 
-    embeddings = get_embeddings(texts)
-    dim = embeddings.shape[1]
+#     embeddings = get_embeddings(texts)
+#     dim = embeddings.shape[1]
 
-    index = faiss.IndexFlatL2(dim)
-    index.add(np.array(embeddings, dtype=np.float32))
+#     index = faiss.IndexFlatL2(dim)
+#     index.add(np.array(embeddings, dtype=np.float32))
 
-    mapping = {i: entry for i, entry in enumerate(rag_entries)}
+#     mapping = {i: entry for i, entry in enumerate(rag_entries)}
 
-    return index, mapping
+#     return index, mapping
+
+def build_index(
+        rag_entries: Union[List[Dict[str,str]], None] = None,
+        user_columns: Union[List[Dict[str, str]], None] = None ) -> Tuple[faiss.IndexFlatL2, Dict[int, Dict[str, str]]]:
+    
+    if user_columns and len(user_columns) > 0:
+        source_entries = user_columns
+    elif rag_entries and len(rag_entries) > 0:
+        source_entries = rag_entries
+    else:
+        raise ValueError("No RAG entries or user columns provided to build index.")
+    
+    texts = [f"{col['name']}: {col['description']}" for col in source_entries]
+
+    
