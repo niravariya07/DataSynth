559240df89c460d183def74748d0422d25f290ee
niravariya07 <nidhiravariya256@gmail.com>
2025-08-14 23:59:54 +0530

    updated : test.py -> saving model locally

--- utils/embedder.py
+++ utils/embedder.py
@@ -2,7 +2,7 @@ from typing import List
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('./models/all-MiniLM-L6-v2')
 
 def get_embedding(text: str) -> np.ndarray:
     return model.encode(text, convert_to_numpy=True)
