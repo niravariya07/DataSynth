35f937b030b29aed572f19294a5f3e8b25c46000
niravariya07 <nidhiravariya256@gmail.com>
2025-08-14 23:56:54 +0530

    updated : test.py -> saving model locally

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import os
 from sentence_transformers import SentenceTransformer
 
 # This will download it locally (default: ~/.cache/torch/sentence_transformers/)
-model_name = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-save_path = "./models/all-MiniLM-L6-v2"
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
 
 model = SentenceTransformer(model_name)
+save_path = "./models/all-MiniLM-L6-v2"
+os.makedirs(save_path, exist_ok=True)
 model.save(save_path)
 
 print(f"Model saved locally at: {save_path}")
