b65c46c922f35aa546701ba5efc26d208c599459
niravariya07 <nidhiravariya256@gmail.com>
2025-08-15 23:36:04 +0530

    updated : embedder.py -> writing function

--- utils/embedder.py
+++ utils/embedder.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from .user_input_parser import user_input_parser
@@ -12,3 +12,14 @@ def get_embedding(user_input: Union[str, dict]) -> np.ndarray:
         parsed_text = user_input
 
     return model.encode(parsed_text, convert_to_numpy=True)
+
+
+def get_embedding_array(user_inputs: List[Union[str, dict]]) -> np.ndarray:
+    parsed_inputs = []
+    for item in user_inputs:
+        if isinstance(item, dict):
+            parsed_inputs.append(user_input_parser(item))
+        else:
+            parsed_inputs.append(item)
+
+    return model.encode(parsed_inputs, convert_to_numpy=True)
