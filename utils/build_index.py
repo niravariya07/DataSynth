71a138d29aa80388d936e2bc7a0587cb8e7c042e
niravariya07 <nidhiravariya256@gmail.com>
2025-08-15 23:37:09 +0530

    updated : build_index.py -> writing function

--- utils/build_index.py
+++ utils/build_index.py
@@ -1,21 +1,25 @@
-from .embedder import get_embedding_array, get_embedding
+from .embedder import get_embedding, get_embedding_array
 from .chunks import chunk_text
+from .user_input_parser import user_input_parser
 import faiss
 import pickle
 from pathlib import Path
-import numpy as np
-from typing import List, Tuple, Dict, Union
+from typing import List
 
 index_file = Path("faiss_index.index")
 metadata_file = Path("metadata.pkl")
 
-def build_faiss_index(columns: List[str]) -> None:
+def build_faiss_index(user_input: str) -> None:
+
+    parsed_data = user_input_parser(user_input)
+    columns = parsed_data["columns"]
+
     all_chunks: List[str] = []
     for col in columns:
         chunk = chunk_text(col)
-        all_chunks.extend(chunk)
+        all_chunks.extend(chunk_text(col))
 
-    embeddings = get_embedding(all_chunks)
+    embeddings = get_embedding_array(all_chunks)
 
     dim = embeddings.shape[1]
     index = faiss.IndexFlatL2(dim)
