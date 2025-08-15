4a03576e4f753f46b65f50db5894c13054b35715
niravariya07 <nidhiravariya256@gmail.com>
2025-08-15 23:30:31 +0530

    updated : chunks.py -> writing function

--- utils/chunks.py
+++ utils/chunks.py
@@ -1,5 +1,13 @@
+from typing import Union
+from .user_input_parser import user_input_parser
 
-def chunk_text(text: str, chunk_size: int =500, overlap: int = 50) -> list[str]:
+def chunk_text(user_input: Union[str, dict], chunk_size: int =500, overlap: int = 50) -> list[str]:
+    
+    if isinstance(user_input, dict):
+        text = user_input_parser(user_input)
+    else:
+        text = user_input
+
     chunks = []
     start = 0
 
