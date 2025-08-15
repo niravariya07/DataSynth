b267b6a52a4121d5ff4a6bd1e5812dbf74d3e0aa
niravariya07 <nidhiravariya256@gmail.com>
2025-08-15 21:59:30 +0530

    updated : build_index

--- utils/build_index.py
+++ utils/build_index.py
@@ -4,23 +4,8 @@ import numpy as np
 from typing import List, Tuple, Dict, Union
 
 def build_index(
-        rag_entries: Union[List[Dict[str,str]], None] = None,
         user_columns: Union[List[Dict[str, str]], None] = None ) -> Tuple[faiss.IndexFlatL2, Dict[int, Dict[str, str]]]:
     
-    if user_columns and len(user_columns) > 0:
-        source_entries = user_columns
-    elif rag_entries and len(rag_entries) > 0:
-        source_entries = rag_entries
-    else:
-        raise ValueError("No RAG entries or user columns provided to build index.")
+    if not user_columns:
+        raise ValueError("user_columns required to build index.")
     
-    texts = [f"{col['name']}: {col['description']}" for col in source_entries]
-
-    embeddings = get_embedding_array(texts)
-    dim = embeddings.shape[1]
-
-    index = faiss.IndexFlatL2(dim)
-    index.add(np.array(embeddings, dtype=np.float32))
-
-    mapping = {i: entry for i, entry in enumerate(source_entries)}
-    return index, mapping
