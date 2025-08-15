834c490148855147f5c8c0a62a33c54679993f06
niravariya07 <nidhiravariya256@gmail.com>
2025-08-15 23:24:13 +0530

    updated : embedder.py -> writing function

--- utils/embedder.py
+++ utils/embedder.py
@@ -6,6 +6,3 @@ model = SentenceTransformer('./models/all-MiniLM-L6-v2')
 
 def get_embedding(text: str) -> np.ndarray:
     return model.encode(text, convert_to_numpy=True)
-
-def get_embedding_array(texts: List[str]) -> np.ndarray:
-    return model.encode(texts, convert_to_numpy=True)
