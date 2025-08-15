0293a99e4b6c7ade179a0e260aa8d13060aded56
niravariya07 <nidhiravariya256@gmail.com>
2025-08-15 22:19:49 +0530

    updated : build_index.py -> import libraries

--- utils/build_index.py
+++ utils/build_index.py
@@ -1,11 +1,9 @@
 from .embedder import get_embedding_array
+from .chunks import chunk_text
 import faiss
+import pickle
+from pathlib import Path
 import numpy as np
 from typing import List, Tuple, Dict, Union
 
-def build_index(
-        user_columns: Union[List[Dict[str, str]], None] = None ) -> Tuple[faiss.IndexFlatL2, Dict[int, Dict[str, str]]]:
-    
-    if not user_columns:
-        raise ValueError("user_columns required to build index.")
-    
+def build_index()
