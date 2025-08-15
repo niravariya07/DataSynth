581581751e4c4a19e5838bc7791f296c232982f2
niravariya07 <nidhiravariya256@gmail.com>
2025-08-15 22:36:04 +0530

    updated : load_index.py -> writing function

--- utils/load_index.py
+++ utils/load_index.py
@@ -1,4 +1,9 @@
+from .build_index import index_file, metadata_file
 from typing import List
 import faiss
 
 def load_faiss_index() -> tuple[faiss.Index,List[str]]:
+    if not index_file.exists() or not metadata_file.exists():
+        raise FileNotFoundError("FaISS index or metadat file not found. Please the index first.")
+    
+    
