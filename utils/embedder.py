cbb81c2bcb7a22660e983d6b0abbd271cc304644
niravariya07 <nidhiravariya256@gmail.com>
2025-08-15 23:27:30 +0530

    updated : embedder.py -> writing function

--- utils/embedder.py
+++ utils/embedder.py
@@ -1,8 +1,14 @@
-from typing import List
+from typing import Union
 from sentence_transformers import SentenceTransformer
 import numpy as np
+from .user_input_parser import user_input_parser
 
 model = SentenceTransformer('./models/all-MiniLM-L6-v2')
 
-def get_embedding(text: str) -> np.ndarray:
-    return model.encode(text, convert_to_numpy=True)
+def get_embedding(user_input: Union[str, dict]) -> np.ndarray:
+    if isinstance(user_input, dict):
+        parsed_text = user_input_parser(user_input)
+    else:
+        parsed_text = user_input
+
+    return model.encode(parsed_text, convert_to_numpy=True)
