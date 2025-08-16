465c8dfc9c7d0215627ab83966082fbd52be288d
niravariya07 <nidhiravariya256@gmail.com>
2025-08-16 17:41:48 +0530

    updated : build_index.py -> removing user_input_parser function call

--- utils/build_index.py
+++ utils/build_index.py
@@ -1,22 +1,18 @@
-from .embedder import get_embedding, get_embedding_array
+from .embedder import get_embedding_array
 from .chunks import chunk_text
-from .user_input_parser import user_input_parser
 import faiss
 import pickle
 from pathlib import Path
 from typing import List, Dict
 
-index_file = Path("faiss_index.index")
-metadata_file = Path("metadata.pkl")
+index_file = Path("/faiss/faiss_index.index")
+metadata_file = Path("/faiss/metadata.pkl")
 
-def build_faiss_index(columns_input: list, raw_row_count: int =10):
-    parsed_data = user_input_parser(columns_input, raw_row_count)
-    columns = parsed_data["columns"]
-
+def build_faiss_index(columns_input: List[str]):
     all_chunks: List[str] = []
-    for col in columns:
-        all_chunks.extend(chunk_text(col["name"]))
-        all_chunks.extend(chunk_text(col["description"]))
+    for col in columns_input:
+        all_chunks.extend(chunk_text(col))
+        
     embeddings = get_embedding_array(all_chunks)
 
     dim = embeddings.shape[1]
