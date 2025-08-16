a8a1e1d4e2739fed43823cf2b0f9fcc93dd8785b
niravariya07 <nidhiravariya256@gmail.com>
2025-08-16 17:37:14 +0530

    updated : embedder.py -> removing user_input_parser function call

--- utils/embedder.py
+++ utils/embedder.py
@@ -1,25 +1,12 @@
-from typing import Union, List
+from typing import List
 from sentence_transformers import SentenceTransformer
 import numpy as np
-from .user_input_parser import user_input_parser
 
 model = SentenceTransformer('./models/all-MiniLM-L6-v2')
 
-def get_embedding(user_input: Union[str, dict]) -> np.ndarray:
-    if isinstance(user_input, dict):
-        parsed_text = user_input_parser(user_input)
-    else:
-        parsed_text = user_input
-
-    return model.encode(parsed_text, convert_to_numpy=True)
+def get_embedding(text: str) -> np.ndarray:
+    return model.encode(text, convert_to_numpy=True)
 
 
-def get_embedding_array(user_inputs: List[Union[str, dict]]) -> np.ndarray:
-    parsed_inputs = []
-    for item in user_inputs:
-        if isinstance(item, dict):
-            parsed_inputs.append(user_input_parser(item))
-        else:
-            parsed_inputs.append(item)
-
-    return model.encode(parsed_inputs, convert_to_numpy=True)
+def get_embedding_array(texts: List[str]) -> np.ndarray:
+    return model.encode(texts, convert_to_numpy=True)
