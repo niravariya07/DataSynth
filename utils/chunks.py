c727950577bbb71103e388d086075555c665985a
niravariya07 <nidhiravariya256@gmail.com>
2025-08-17 21:12:21 +0530

    updated : chunks.py

--- utils/chunks.py
+++ utils/chunks.py
@@ -6,8 +6,7 @@ def chunk_text(text: str, chunk_size: int =500, overlap: int = 50) -> list[str]:
 
     while start < len(text):
         end = start + chunk_size
-        chunk = text[start:end]
-        chunks.append(chunk)
+        chunks.append(text[start:end])
         start += chunk_size - overlap
 
     return chunks
