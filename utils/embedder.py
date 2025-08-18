9d1cd2122f205330fdb4a1e356e57c24727d83ca
niravariya07 <nidhiravariya256@gmail.com>
2025-08-18 23:42:57 +0530

    updated : embedder.py

--- utils/embedder.py
+++ utils/embedder.py
@@ -1,12 +1,10 @@
-from typing import List
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
 model = SentenceTransformer('./models/all-MiniLM-L6-v2')
 
-def get_embedding(text: str) -> np.ndarray:
+def get_embedding(text):
     return model.encode(text, convert_to_numpy=True)
 
-
-def get_embedding_array(texts: List[str]) -> np.ndarray:
+def get_embedding_array(texts):
     return model.encode(texts, convert_to_numpy=True)
