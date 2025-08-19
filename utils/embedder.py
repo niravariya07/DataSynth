e3f1abeabaaecb1c105a938fc4fb289f27abf189
niravariya07 <nidhiravariya256@gmail.com>
2025-08-19 19:37:02 +0530

    updated : embedder.py

--- utils/embedder.py
+++ utils/embedder.py
@@ -1,7 +1,7 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+model = SentenceTransformer('./models/all-MiniLM-L6-v2')
 
 def get_embedding(text):
     return model.encode(text, convert_to_numpy=True)
