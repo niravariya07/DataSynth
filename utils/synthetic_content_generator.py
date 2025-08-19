ed569259293bf0f23ff4ff4ffcf128b00d447018
niravariya07 <nidhiravariya256@gmail.com>
2025-08-19 18:30:39 +0530

    updated : synthetic_content_generator.py

--- utils/synthetic_content_generator.py
+++ utils/synthetic_content_generator.py
@@ -10,7 +10,7 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 def data_generator_llm(columns, num_rows):
     
     col_descriptions = ", ".join([
-    f"{col.get('name', '')}: {col.get('type_hint', 'string')}"
+    f"{col.get('name', '')}: {col.get('description', 'string')}"
     if isinstance(col, dict) else str(col)
     for col in columns])
 
