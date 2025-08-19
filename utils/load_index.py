36ff66b180b8712e97d48c3b1dc49c77f433d59d
niravariya07 <nidhiravariya256@gmail.com>
2025-08-19 16:44:57 +0530

    updated : load_index.py

--- utils/load_index.py
+++ utils/load_index.py
@@ -1,14 +1,13 @@
 import pickle
 from .build_index import index_file, metadata_file
-from typing import List, Tuple
 import faiss
 
-def load_faiss_index() -> Tuple[faiss.Index,List[str]]:
+def load_faiss_index():
     if not index_file.exists() or not metadata_file.exists():
         raise FileNotFoundError("FaISS index or metadat file not found. Please the index first.")
     
     index = faiss.read_index(str(index_file))
     with open(metadata_file, "rb") as f:
-        metadata: List[str] = pickle.load(f)
+        metadata = pickle.load(f)
     
     return index, metadata
